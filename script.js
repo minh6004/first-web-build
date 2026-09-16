0f2ad1359ef35e37a538e9bf807a1ca72b7104d5
minha6004 <aiuni5263@example.com>
2026-09-16 12:45:13 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const navToggle = document.getElementById("navToggle");
+const navList = document.getElementById("navList");
+
+navToggle.addEventListener("click", () => {
+  const isOpen = navList.classList.toggle("is-open");
+  navToggle.setAttribute("aria-expanded", String(isOpen));
+});
